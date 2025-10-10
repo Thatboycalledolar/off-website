f0dd6cf2d4b1e42b25d8eee1e20c20f8edd5c360
olar <kaffoolar@gmail.com>
2025-10-10 19:42:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const menuIcon = document.getElementById('menuIcon');
+const navLinks = document.getElementById('navLinks');
+
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  menuIcon.classList.toggle('active');
+});
+
+
